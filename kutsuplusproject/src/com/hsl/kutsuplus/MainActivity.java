6cab4d290333a7cab07a238747857845cc54f877
Mailis Toompuu <maiilu@hotmail.com>
2014-02-14 13:22:59 +0200

    wrote a comment, mailis

--- kutsuplusproject/src/com/hsl/kutsuplus/MainActivity.java
+++ kutsuplusproject/src/com/hsl/kutsuplus/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity
 {
+	//hellou!!!
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
