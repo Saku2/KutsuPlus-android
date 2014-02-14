1988c9ab8ac8c2d7e9469ae8318774411957ae4e
Sakari Tanskanen <sakari.tanskanen@gmail.com>
2014-02-14 14:30:47 +0200

    Change package from com.hsl.kutsuplus to fi.aalto.kutsuplus

---
+++ kutsuplusproject/src/fi/aalto/kutsuplus/FormFragment.java
@@ -0,0 +1,17 @@
+package fi.aalto.kutsuplus;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FormFragment extends Fragment {
+
+	private View rootView
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		rootView = inflater.inflate(R.layout.formfragment, container, false);
+	}
+	
+}
---
+++ kutsuplusproject/src/fi/aalto/kutsuplus/MainActivity.java
@@ -0,0 +1,46 @@
+package com.hsl.kutsuplus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar.Tab;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity extends ActionBarActivity implements android.support.v7.app.ActionBar.TabListener
+{
+
+    /** Called when the activity is first created. */
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        
+
+    }
+
+	@Override
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabSelected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
