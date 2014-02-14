1bf5cc6b1f89eb829a68d8b1588e6daf471b438a
Mailis Toompuu <maiilu@hotmail.com>
2014-02-14 15:19:02 +0200

    distinguish hone and tablet sizes..
    ... in MainActivity OnCreate method

--- kutsuplusproject/src/fi/aalto/kutsuplus/MainActivity.java
+++ kutsuplusproject/src/fi/aalto/kutsuplus/MainActivity.java
@@ -26,6 +26,44 @@ public class MainActivity extends ActionBarActivity implements android.support.v
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         
 
+        //web from:
+        	//http://stackoverflow.com/questions/15533343/android-fragment-basics-tutorial
+        // Check whether the activity is using the layout version with
+		// the fragment_container FrameLayout. If so, we must add the first
+		// fragment
+        //ONE-PANE LAYOUT
+		if (findViewById(R.id.phone_fragment_container) != null) {
+
+			// However, if we're being restored from a previous state,
+			// then we don't need to do anything and should return or else
+			// we could end up with overlapping fragments.
+			if (savedInstanceState != null) {
+				return;
+			}
+
+			// Create an instance of ExampleFragment
+			FormFragment formFragment = new FormFragment();
+
+			// In case this activity was started with special instructions from
+			// an Intent,
+			// pass the Intent's extras to the fragment as arguments
+			formFragment.setArguments(getIntent().getExtras());
+
+			// Add the fragment to the 'fragment_container' FrameLayout
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.phone_fragment_container, formFragment).commit();
+		}
+		//TWO-PANE LAYOUT
+		else{//in two-pane layout set general as initial detail view
+			// Capture the detail fragment from the activity layout
+			MapFragment mapFrag = (MapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map_fragment);
+			if (mapFrag != null) {
+				//some view-changing method in map-fragmet
+				//mapFrag.updateDetailView(0);
+			}
+		}
+     		
     }
 
 	@Override
