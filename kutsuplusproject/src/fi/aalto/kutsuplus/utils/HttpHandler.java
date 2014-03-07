89c33507be4dee7b46dc1bc5ee7dca2060aa826b
Sakari Tanskanen <sakari.tanskanen@gmail.com>
2014-03-07 16:25:07 +0200

    Added http-get utility class

---
+++ kutsuplusproject/src/fi/aalto/kutsuplus/utils/HttpHandler.java
@@ -0,0 +1,52 @@
+package fi.aalto.kutsuplus.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class HttpHandler {
+	
+	public HttpHandler() {
+	
+	}
+	
+	public String makeHttpGet(String url, List<NameValuePair> params) {
+		
+		String response = "";
+		try {
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpEntity httpEntity = null;
+			HttpResponse httpResponse = null;
+			
+			InputStream content = null;
+			if (params != null) {
+				String paramString = URLEncodedUtils
+						.format(params, "utf-8");
+				url += "?" + paramString;
+		    }
+		    HttpGet httpGet = new HttpGet(url);
+		    httpResponse = httpClient.execute(httpGet);
+		    httpEntity = httpResponse.getEntity();
+		   	response = EntityUtils.toString(httpEntity);
+		} catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		return response;
+	}
+	  
+}
