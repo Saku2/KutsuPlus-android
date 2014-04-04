2b8498230e3e7199bf5438bf2e92e243db19cbb4
Sakari Tanskanen <sakari@Sakaris-MacBook-Air.local>
2014-04-04 13:30:45 +0300

    Moved reittiopas api get to AsyncTask

--- kutsuplusproject/src/fi/aalto/kutsuplus/utils/HttpHandler.java
+++ kutsuplusproject/src/fi/aalto/kutsuplus/utils/HttpHandler.java
@@ -2,7 +2,9 @@ package fi.aalto.kutsuplus.utils;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -14,6 +16,8 @@ import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import android.os.AsyncTask;
+
 public class HttpHandler {
 
 	public HttpHandler() {
@@ -23,29 +27,52 @@ public class HttpHandler {
 	public String makeHttpGet(String url, List<NameValuePair> params) {
 
 		String response = "";
+		ReittiopasGetTask task = new ReittiopasGetTask();
+		task.execute(url, params);
 		try {
-			HttpClient httpClient = new DefaultHttpClient();
-			HttpEntity httpEntity = null;
-			HttpResponse httpResponse = null;
-
-			if (params != null) {
-				String paramString = URLEncodedUtils.format(params, "utf-8");
-				url += "?" + paramString;
-			}
-			HttpGet httpGet = new HttpGet(url);
-			httpResponse = httpClient.execute(httpGet);
-			httpEntity = httpResponse.getEntity();
-			response = EntityUtils.toString(httpEntity);
-		} catch (UnsupportedEncodingException e) {
+			response = task.get();
+		} catch (InterruptedException e) {
 			e.printStackTrace();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
+		} catch (ExecutionException e) {
 			e.printStackTrace();
 		}
 		return response;
+		
 	}
+	
+	private class ReittiopasGetTask extends AsyncTask<Object, Integer, String> {
+	     protected String doInBackground(Object... args) {
+	 		String response = "";
+			try {
+				HttpClient httpClient = new DefaultHttpClient();
+				HttpEntity httpEntity = null;
+				HttpResponse httpResponse = null;
+				String url = (String)args[0];
+				List<NameValuePair> params = (List<NameValuePair>)args[1];
+				if (params != null) {
+					String paramString = URLEncodedUtils.format(params, "utf-8");
+					url += "?" + paramString;
+				}
+				HttpGet httpGet = new HttpGet(url);
+				httpResponse = httpClient.execute(httpGet);
+				httpEntity = httpResponse.getEntity();
+				response = EntityUtils.toString(httpEntity);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return response;
+	     }
 
+	     protected void onProgressUpdate(Integer... progress) {
+	     }
+
+	     protected void onPostExecute(String result) {
+	     }
+	 }
 }
